0ea5a8ddbefad151f10944c6c0a2f3b7174d286c
ASadSexyImp <masakazuplus@gmail.com>
2019-09-11 15:18:48 +0900

    shake test

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
+var shakes = 0;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(0);
+  background(shakes);
 
   if (mouseIsPressed) {
     fill(255);
     ellipse(mouseX, mouseY, 50, 50);
+  }
+
+
+}
+
+function deviceShaken() {
+  shakes = shakes + 1;
+  if (shakes > 255) {
+    shakes = 0;
   }
 }
