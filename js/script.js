f09b1573d77b102dec16a1517ec46f41fab82b02
ASadSexyImp <masakazuplus@gmail.com>
2019-09-11 17:03:20 +0900

    change image location

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var img;
-var NUM = 60;
+var NUM = 600;
 var x = [];
 var y = [];
 var r = [];
-var colg;
+var colr;
 var colb;
 
-var shakes = 0;
+// var shakes = 0;
 
 function preload() {
   img = loadImage('helloworld.png');
@@ -28,30 +28,30 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // prepare to background
-  frameRate(10);
+  frameRate(60);
   background(0);
   for (var i = 0; i < NUM; i++) {
     x[i] = 0;
-    y[i] = random(10, height - 10);
-    r[i] = random(10, 100);
+    y[i] = random(height / 2 - 200, height / 2 + 200);
+    r[i] = random(10, 30);
   }
+  imageMode(CENTER);
 }
 
-function draw() {
+// function draw() {
 
-
-}
+// }
 
 // shaken
 function deviceShaken() {
   for (var i = 0; i < NUM; i++) {
-    colg = map(x[i], 0, 500, 50, 150);
+    colr = map(x[i], 0, 500, 50, 150);
     colb = map(y[i], 0, 500, 50, 150);
-    fill(random(200, 250), colg, colb, 50);
+    fill(colr, random(200, 250), colb, 50);
     noStroke();
     ellipse(x[i], y[i], r[i]);
     x[i] = x[i] + random(10);
     y[i] = y[i] + random(-5, 5);
   }
-  image(img, 0, 0);
+  image(img, width / 2, height / 2);
 }
