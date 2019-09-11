1ea2cfa57e55f2e5fc1dc0b08d7ec76bd296ecc2
ASadSexyImp <masakazuplus@gmail.com>
2019-09-11 14:59:21 +0900

    test mouse event

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+
+  if (mouseIsPressed) {
+    fill(255);
+    ellipse(mouseX, mouseY, 50, 50);
+  }
+}
