d15bf51de01c2757da2d684af251b2c0510a1b17
ASadSexyImp <masakazuplus@gmail.com>
2019-09-12 13:59:48 +0900

    2nd shake

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+// shake
+var shakes = 0;
+
+// helloworld
 var img;
 var NUM = 600;
 var x = [];
@@ -6,7 +10,9 @@ var r = [];
 var colr;
 var colb;
 
-// var shakes = 0;
+// sparkling
+var sparklings = [];
+
 
 function preload() {
   img = loadImage('helloworld.png');
@@ -39,19 +45,78 @@ function setup() {
 }
 
 function draw() {
+  // hello world
+  if (shakes < 100) {
 
+    for (var i = 0; i < NUM; i++) {
+      colr = map(x[i], 0, 500, 50, 150);
+      colb = map(y[i], 0, 500, 50, 150);
+      fill(colr, random(200, 250), colb, 50);
+      noStroke();
+      ellipse(x[i], y[i], r[i]);
+      x[i] = x[i] + random(10);
+      y[i] = y[i] + random(-5, 5);
+    }
+    image(img, width / 2, height / 2);
+
+  }
+  // sparkling
+  else if (shakes < 200) {
+    background(255);
+    blendMode(OF_BLENDMODE_MULTIPLY);
+
+    for (var i = 0; i < sparklings.length; i++) {
+
+      fill(random(50, 150), 240, 230);
+      if (sparklings[i].lifetime < 0) {
+        sparklings.splice(i, 1);
+      } else {
+        sparklings[i].draw();
+        sparklings[i].move();
+      }
+    }
+
+  }
 }
 
 // shaken
 function deviceShaken() {
-  for (var i = 0; i < NUM; i++) {
-    colr = map(x[i], 0, 500, 50, 150);
-    colb = map(y[i], 0, 500, 50, 150);
-    fill(colr, random(200, 250), colb, 50);
-    noStroke();
-    ellipse(x[i], y[i], r[i]);
-    x[i] = x[i] + random(10);
-    y[i] = y[i] + random(-5, 5);
+  shakes++;
+
+  if (shakes > 100) {
+    var pos = createVector(random(width), random(height));
+    for (var i = 0; i < 30; i++) {
+      sparklings.push(new Sparkling(pos));
+    }
   }
-  image(img, width / 2, height / 2);
+}
+
+class Sparkling {
+  // 初期化（constructor＝コンストラクター＝建設者）
+  constructor(pos) {
+    // 花火本体の位置で初期化
+    this.position = createVector(pos.x, pos.y);
+    // 飛び散る向きをランダムに決める
+    this.velocity = createVector(ofRandom(-3, 3), ofRandom(-3, 3)); // 速度
+
+    this.radius = random(5, 20);
+    // 花火の粒が表示される時間をランダムに決める
+    this.lifetime = random(120, 180);
+  }
+
+  // 移動
+  move() {
+    // 空気抵抗
+    this.velocity.mult(0.98);
+    // 速度で座標が変化する
+    this.position.add(this.velocity);
+    // 残りの時間を減らす
+    this.lifetime--;
+  }
+
+  // 表示
+  draw() {
+    // 小さな円を描く
+    ellipse(this.position.x, this.position.y, 2);
+  }
 }
