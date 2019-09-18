30b627c38560da377a27dfe0cf04d740bb968801
ASadSexyImp <masakazuplus@gmail.com>
2019-09-18 11:29:49 +0900

    debug

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ function draw() {
 
 
   if (mode == "helloworld" && isShake) {
-
+    background(255);
     for (var i = 0; i < 600; i++) {
       // decide color
       var colr = map(pos[i].x, 0, 500, 50, 150);
