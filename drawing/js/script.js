db34b524a667f39e4e3999c323038f3f5bfb7dcb
ASadSexyImp <masakazuplus@gmail.com>
2019-09-27 13:22:34 +0900

    drawing

---
+++ drawing/js/script.js
@@ -0,0 +1,30 @@
+// https://editor.p5js.org/dano/sketches/Sk1UY7tee のコードを元にしています
+
+// カラー配列変数
+var colors;
+
+function setup() {
+  // キャンバス作成
+  createCanvas(windowWidth, windowHeight);
+  // 背景作成
+  background(200);
+  // 枠なし
+  noStroke();
+  // カラー配列に入れる
+  colors = [color(255, 0, 0), color(0, 255, 0), color(0, 0, 255), color(255, 255, 0), color(0, 255, 255)];
+}
+
+function draw() {
+  // タッチの分だけ
+  for (var i = 0; i < touches.length; i++) {
+    // 色つける
+    fill(colors[i]);
+    // 円を描く
+    ellipse(touches[i].x, touches[i].y, 24, 24);
+  }
+}
+
+// スクリーンが動くのを防ぐ
+function touchMoved() {
+  return false;
+}
